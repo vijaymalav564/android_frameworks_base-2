009712cebec2d8c719ac86c00032ce8475a9ec32
Joe Maples <joe@frap129.org>
2017-01-17 11:39:07 -0500

    Reload Pixel Home Animation without reboot
    Change-Id: Ic15c7e75c762465229f078939232312ee28cacf3
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/google/android/systemui/OpaLayout.java
+++ packages/SystemUI/src/com/google/android/systemui/OpaLayout.java
@@ -6,9 +6,13 @@ import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.content.res.Resources;
+import android.content.ContentResolver;
+import android.database.ContentObserver;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.graphics.PorterDuff;
+import android.net.Uri;
+import android.os.Handler;
 import android.os.SystemClock;
 import android.os.UserHandle;
 import android.os.UserManager;
@@ -97,6 +101,26 @@ public class OpaLayout extends FrameLayout implements ButtonInterface{
     private final Interpolator mDotsFullSizeInterpolator;
     private final Interpolator mFastOutSlowInInterpolator;
     private final Interpolator mHomeDisappearInterpolator;
+    private SettingsObserver mSettingsObserver;
+
+    protected class SettingsObserver extends ContentObserver {
+        SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+           ContentResolver resolver = mContext.getContentResolver();
+           resolver.registerContentObserver(Settings.System.getUriFor(
+                  Settings.System.PIXEL_NAV_ANIMATION),
+                  false, this, UserHandle.USER_CURRENT);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+           super.onChange(selfChange, uri);
+           setOpaEnabled(true);
+        }
+    }
 
     private OverviewProxyService mOverviewProxyService;
 
@@ -129,6 +153,10 @@ public class OpaLayout extends FrameLayout implements ButtonInterface{
         mAnimationState = OpaLayout.ANIMATION_STATE_NONE;
         mCurrentAnimators = new ArraySet<Animator>();
         mOverviewProxyService = Dependency.get(OverviewProxyService.class);
+        if (mSettingsObserver == null) {
+            mSettingsObserver = new SettingsObserver(new Handler());
+        }
+        mSettingsObserver.observe();
     }
 
     public OpaLayout(Context context, AttributeSet attrs) {
@@ -160,6 +188,10 @@ public class OpaLayout extends FrameLayout implements ButtonInterface{
         mAnimationState = OpaLayout.ANIMATION_STATE_NONE;
         mCurrentAnimators = new ArraySet<Animator>();
         mOverviewProxyService = Dependency.get(OverviewProxyService.class);
+        if (mSettingsObserver == null) {
+            mSettingsObserver = new SettingsObserver(new Handler());
+        }
+        mSettingsObserver.observe();
     }
 
     public OpaLayout(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -191,6 +223,10 @@ public class OpaLayout extends FrameLayout implements ButtonInterface{
         mAnimationState = OpaLayout.ANIMATION_STATE_NONE;
         mCurrentAnimators = new ArraySet<Animator>();
         mOverviewProxyService = Dependency.get(OverviewProxyService.class);
+        if (mSettingsObserver == null) {
+            mSettingsObserver = new SettingsObserver(new Handler());
+        }
+        mSettingsObserver.observe();
     }
 
     public OpaLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
@@ -221,6 +257,10 @@ public class OpaLayout extends FrameLayout implements ButtonInterface{
         };
         mAnimationState = OpaLayout.ANIMATION_STATE_NONE;
         mCurrentAnimators = new ArraySet<Animator>();
+        if (mSettingsObserver == null) {
+            mSettingsObserver = new SettingsObserver(new Handler());
+        }
+        mSettingsObserver.observe();
     }
 
     private void startAll(ArraySet<Animator> animators) {
