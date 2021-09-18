crdroidandroid/android_frameworks_base

Add toggle to disable HW keys [1/2]
* This is rewrite based on DU implementation using LOS CMHardware API.

Ref commits:
https://github.com/DirtyUnicorns/android_frameworks_base/commit/dd052fe80c47f3dd5b446302117241a6ff69b01c
https://github.com/DirtyUnicorns/android_frameworks_base/commit/fe24436e314cfe20fbd58513503ca048d026a8d5

Change-Id: I793352dc9b0700890546d53ee7c4231cd76afd44
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -713,6 +713,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     // User defined bar visibility, regardless of factory configuration
     boolean mNavbarVisible = false;
+
+    // User defined hw key config
+    boolean mHardwareKeysDisable = false;
+
     // Pie
     boolean mPieState;
 
@@ -1182,6 +1186,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.ANBI_ENABLED), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.Secure.getUriFor(
+                    Settings.Secure.HARDWARE_KEYS_DISABLE), false, this,
                     UserHandle.USER_ALL);
             updateSettings();
         }
@@ -2560,6 +2567,17 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             mShortPressWindowBehavior = SHORT_PRESS_WINDOW_PICTURE_IN_PICTURE;
         }
 
+        if (mHardwareKeysDisable) {
+            mLongPressOnHomeBehavior = KEY_ACTION_NOTHING;
+            mDoubleTapOnHomeBehavior = KEY_ACTION_NOTHING;
+            mPressOnMenuBehavior = KEY_ACTION_NOTHING;
+            mLongPressOnMenuBehavior = KEY_ACTION_NOTHING;
+            mPressOnAssistBehavior = KEY_ACTION_NOTHING;
+            mLongPressOnAssistBehavior = KEY_ACTION_NOTHING;
+            mPressOnAppSwitchBehavior = KEY_ACTION_NOTHING;
+            mLongPressOnAppSwitchBehavior = KEY_ACTION_NOTHING;
+        }
+
         mNavBarOpacityMode = res.getInteger(
                 com.android.internal.R.integer.config_navBarOpacityMode);
     }
@@ -2745,20 +2763,19 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 updateWakeGestureListenerLp();
             }
 
-            boolean doShowNavbar = Settings.Secure.getIntForUser(resolver,
+            mNavbarVisible = Settings.Secure.getIntForUser(resolver,
                     Settings.Secure.NAVIGATION_BAR_VISIBLE,
                     DUActionUtils.hasNavbarByDefault(mContext) ? 1 : 0,
                     UserHandle.USER_CURRENT) == 1;
-            if (doShowNavbar != mNavbarVisible) {
-                mNavbarVisible = doShowNavbar;
-/*
-                if (mCMHardware.isSupported(CMHardwareManager.FEATURE_KEY_DISABLE)) {
-                    mCMHardware.set(CMHardwareManager.FEATURE_KEY_DISABLE, mNavbarVisible);
-                }
-*/
-            }
 
             updateNavigationBarSize();
+
+            mHardwareKeysDisable = Settings.Secure.getIntForUser(resolver,
+                    Settings.Secure.HARDWARE_KEYS_DISABLE,
+                    DUActionUtils.hasNavbarByDefault(mContext) ? 1 : 0,
+                    UserHandle.USER_CURRENT) == 1;
+
+            mCMHardware.set(CMHardwareManager.FEATURE_KEY_DISABLE, mHardwareKeysDisable);
 
             updateKeyAssignments();
 

vijaymalav564/android_frameworks_base-2

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -561,7 +561,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     int mMetaState;
     int mInitialMetaState;
 
-    private int mForceNavbar = -1;
+    // User defined hw key config
+    boolean mHardwareKeysDisable = false;
 
     // Tracks user-customisable behavior for certain key events
     private Action mHomeLongPressAction;
@@ -895,9 +896,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     Settings.Global.POWER_BUTTON_SUPPRESSION_DELAY_AFTER_GESTURE_WAKE), false, this,
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(LineageSettings.System.getUriFor(
-                    LineageSettings.System.FORCE_SHOW_NAVBAR), false, this,
-                    UserHandle.USER_ALL);
-            resolver.registerContentObserver(LineageSettings.System.getUriFor(
                     LineageSettings.System.TORCH_LONG_PRESS_POWER_GESTURE), false, this,
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(LineageSettings.System.getUriFor(
@@ -960,7 +958,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.ANBI_ENABLED), false, this,
                     UserHandle.USER_ALL);
-
+            resolver.registerContentObserver(Settings.Secure.getUriFor(
+                    Settings.Secure.HARDWARE_KEYS_DISABLE), false, this,
+                    UserHandle.USER_ALL);
             updateSettings();
         }
 
@@ -2291,7 +2291,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private void updateKeyAssignments() {
         int activeHardwareKeys = mDeviceHardwareKeys;
 
-        if (mForceNavbar == 1) {
+        if (mHardwareKeysDisable) {
             activeHardwareKeys = 0;
         }
 
@@ -2483,15 +2483,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 }
             }
 
-            int forceNavbar = LineageSettings.System.getIntForUser(resolver,
-                    LineageSettings.System.FORCE_SHOW_NAVBAR, 0,
-                    UserHandle.USER_CURRENT);
-            if (forceNavbar != mForceNavbar) {
-                mForceNavbar = forceNavbar;
-                if (mLineageHardware.isSupported(LineageHardwareManager.FEATURE_KEY_DISABLE)) {
-                    mLineageHardware.set(LineageHardwareManager.FEATURE_KEY_DISABLE,
-                            mForceNavbar == 1);
-                }
+            if (mLineageHardware.isSupported(LineageHardwareManager.FEATURE_KEY_DISABLE)) {
+                mHardwareKeysDisable = Settings.Secure.getIntForUser(resolver,
+                        Settings.Secure.HARDWARE_KEYS_DISABLE, 0,
+                        UserHandle.USER_CURRENT) == 1;
+                mLineageHardware.set(LineageHardwareManager.FEATURE_KEY_DISABLE, mHardwareKeysDisable);
             }
 
             updateKeyAssignments();