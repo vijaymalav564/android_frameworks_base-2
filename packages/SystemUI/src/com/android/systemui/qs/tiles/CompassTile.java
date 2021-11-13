Havoc-OS/android_frameworks_base

CompassTile: Add a device supports compass check
- Previously it showed up on devices even without compass support
  making it a bloat

Change-Id: I79d37b90255e0d674e73e45452a510c8d990e9dd
Signed-off-by: PMS22 <prathams99@rediff.com>

packages/SystemUI/src/com/android/systemui/qs/tiles/CompassTile.java
@@ -28,6 +28,7 @@ import android.widget.ImageView;
 
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.util.havoc.HavocUtils;
 import com.android.systemui.R;
 import com.android.systemui.plugins.qs.QSIconView;
 import com.android.systemui.plugins.qs.QSTile.BooleanState;
@@ -74,6 +75,11 @@ public class CompassTile extends QSTileImpl<BooleanState> implements SensorEvent
         QSIconView iconView = super.createTileView(context);
         mImage = (ImageView) iconView.findViewById(android.R.id.icon);
         return iconView;
+    }
+	
+	@Override
+    public boolean isAvailable() {
+        return HavocUtils.deviceHasCompass(mContext);
     }
 
     @Override

vijaymalav564/android_frameworks_base-2

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/tiles/CompassTile.java
@@ -27,6 +27,7 @@ import android.widget.ImageView;
 
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.util.crdroid.Utils;
 import com.android.systemui.R;
 import com.android.systemui.plugins.qs.QSIconView;
 import com.android.systemui.plugins.qs.QSTile.BooleanState;
@@ -160,6 +161,11 @@ public class CompassTile extends QSTileImpl<BooleanState> implements SensorEvent
         } else {
             return mContext.getString(R.string.accessibility_quick_settings_compass_off);
         }
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return Utils.deviceHasCompass(mContext);
     }
 
     @Override