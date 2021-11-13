PixelExperience/frameworks_base

fwb: Add support for internal audio recording
Change-Id: I953c07d9a511b53ddb79f6a7ce5b47ca0c079acb

core/java/com/android/internal/util/custom/recorder/InternalAudioRecorder.java
@@ -1,0 +1,24 @@
+/*
+* Copyright (C) 2017 The Pixel Experience Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.android.internal.util.custom.recorder;
+
+import android.content.Context;
+
+public class InternalAudioRecorder {
+    public static boolean isSupported(Context context) {
+        return context.getResources().getBoolean(com.android.internal.R.bool.config_hasInternalAudioRecordingSupport);
+    }
+}

vijaymalav564/android_frameworks_base-2

Signed-off-by: DennySPB <dennyspb@gmail.com>

core/java/com/android/internal/util/crdroid/recorder/InternalAudioRecorder.java
@@ -1,0 +1,24 @@
+/*
+* Copyright (C) 2017 The Pixel Experience Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.android.internal.util.crdroid.recorder;
+
+import android.content.Context;
+
+public class InternalAudioRecorder {
+    public static boolean isSupported(Context context) {
+        return context.getResources().getBoolean(com.android.internal.R.bool.config_hasInternalAudioRecordingSupport);
+    }
+}