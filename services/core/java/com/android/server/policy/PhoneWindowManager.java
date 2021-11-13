BlissRoms/platform_frameworks_base

base: Add more custom actions [1/3]
- QS panel
- Ringer modes

- Reararnge modes / Call actions directly from ActionUtils

Change-Id: I95a7caebfd324c90241045b98599b4a059185110
Signed-off-by: rohan <purohit.rohan@gmail.com>

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1963,6 +1963,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case NOTIFICATIONS:
                 BlissUtils.toggleNotifications();
                 break;
+            case QS_PANEL:
+                BlissUtils.toggleQsPanel();
+                break;
+            case RINGER_MODES:
+                BlissUtils.toggleRingerModes(mContext);
+                break;
             default:
                 break;
         }

vijaymalav564/android_frameworks_base-2

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1961,6 +1961,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case NOTIFICATIONS:
                 Utils.toggleNotifications();
                 break;
+            case QS_PANEL:
+                Utils.toggleQsPanel();
+                break;
+            case RINGER_MODES:
+                Utils.toggleRingerModes(mContext);
+                break;
             default:
                 break;
         }